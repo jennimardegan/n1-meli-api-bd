a2b9561812f713c29d8baf782716857ebab2c63d
jennimardegan <jennifer_mardegan@hotmail.com>
2019-11-11 22:16:40 -0300

    Aula 01

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const app = express()
+
+//conexão com o MongoDB local
+mongoose.connect("mongodb://localhost:27017/reprograma", {useNewUrlParser:true});
+
+let db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:")) //erro caso problema de conexão com mongo, mostrando no console
+db.once("open", function(){
+  console.log("conexão feita com sucesso.") //uma vez conectado ao mongo, aparece essa mensagem
+})
 
 //rotas
 const index = require("./routes/index")
---
+++ src/model/alunas.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const AlunasSchema = new mongoose.Schema({ //resumo das características que tem o documento
+    nome: { type: String },
+    dateOfBirth: { type: String }, //Date se não fosse string
+    nasceuEmSp: { type: String }, //Boolean se não fosse string
+    //id: {}
+    livros: [{ titulo: String, leu: String }] //Boolean se não fosse string
+})
+
+//Exemplo para deixar o parametro como obrigatório:
+// var sampleSchema = new Schema({ name: {type: String, required: true}})
